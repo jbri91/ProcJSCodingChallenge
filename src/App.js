b8138fe946bf784fe8a4a9542f942672d0e5b496
Johnny Briones <jbrrii91@gmail.com>
2021-11-09 23:50:38 -0500

    fetch from brewery api and render on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react' 
+import Navigation from './Components/Navigation'
 import './App.css';
 
 function App() {
+
+  const [brewery, setBrewery] = useState([])
+  const [city, setCity] = useState('New York')
+useEffect(() => {
+ fetchData();
+
+}, []) 
+
+async function fetchData() {
+  const response = await fetch('https://api.openbrewerydb.org/breweries?by_city=new_york');
+  const breweries = await  response.json()
+  setBrewery(breweries)
+}
+
+
+let breweryByCity = []
+for (let i =0; i < brewery.length; i ++) {
+  breweryByCity.push({
+    id: brewery[i].id,
+    name: brewery[i].name,
+    city: brewery[i].city,
+    country: brewery[i].country,
+    phone: brewery[i].phone,
+    state: brewery[i].state,
+    street: brewery[i].street,
+    url: brewery[i].website_url
+  })
+}
+
+
+
   return (
     <div className="App">
+      <Navigation/>
       <header className="App-header">
-        <p> API Call</p>
+        <input type='text' name='state' placeholder='Search By State' onChange={e => setCity(e.target.value)}></input>
+        <br/>
+        <h2> List of Breweries In {city} </h2> 
+        {breweryByCity.map(function(b, idx){
+          return (
+          <li key={idx}>{b.name}</li>
+          )
+        })}
       </header>
+      <ul>
+    </ul>
     </div>
   );
 }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,9 @@
+// import { Navbar, Button, Container } from "react-bootstrap";
+
+function Navigation() {
+    return (
+    <h1>Breweries</h1>
+    )
+}
+
+export default Navigation
